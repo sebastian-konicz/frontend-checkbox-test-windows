c5add5d08cf5c65eb94bed065bbcdc6b4fa1eddb
Sebastian Konicz <sebastian.konicz@gmail.com>
2018-08-21 12:55:18 +0200

    Zadanie rekrutacyjne

--- src/modules/checkbox/index.js
+++ src/modules/checkbox/index.js
@@ -2,14 +2,40 @@
 import React from 'react';
 
 class Checkbox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checkbox: true,
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+           checkbox: !this.state.checkbox
+        });
+    };
+
     render() {
+        let isVisible = "visuallyhidden"
+        if(this.state.checkbox == false){
+            isVisible = "visible"
+        }
+
         return(
             <div className="checkbox">
                 <div className="title">
                     Checkbox
                 </div>
                 <div className="content">
-                    <input type="checkbox" />
+                    <label className="switch">
+                        <input type="checkbox"
+                           onChange={this.handleChange}/>
+                        <div className="slider round"></div>
+                        <div className="slider"></div>
+                    </label>
+                </div>
+                <div className={`hiddenContent ${isVisible}`}>
+                    hidden content
                 </div>
             </div>
         );
